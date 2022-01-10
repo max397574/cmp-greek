59f8d5c755bc1b833b4838676c1120a31699c148
max397574 <max.heinzer397@gmx.ch>
2022-01-10 09:13:14 +0100

    initial commit

---
+++ lua/cmp_greek/items.lua
@@ -0,0 +1,38 @@
+return function()
+  return {
+    { word = ":alpha:", label = "α :alpha:", insertText = "α", filterText = ":alpha:" },
+    { word = ":Alpha:", label = "Α :Alpha:", insertText = "α", filterText = ":Alpha:" },
+    { word = ":beta:", label = "β :beta:", insertText = "β", filterText = ":beta:" },
+    { word = ":Beta:", label = "Β :Beta:", insertText = "Β", filterText = ":Beta:" },
+    { word = ":gamma:", label = ":γ gamma:", insertText = "γ", filterText = ":gamma:" },
+    { word = ":Gamma:", label = ":Γ Gamma:", inertText = "Γ", filterText = ":Gamma:" },
+    { word = ":delta:", label = ":δ delta:", insertText = "δ", filterText = ":delta:" },
+    { word = ":Delta:", label = ":Δ Delta:", insertText = "Δ", filterText = ":Delta:" },
+    { word = ":epsilon:", label = ":ε epsilon:", insertText = "ε", filterText = ":epsilon:" },
+    { word = ":Epsilon:", label = ":Ε Epsilon:", insertText = "Ε", filterText = ":Epsilon:" },
+    { word = ":zeta:", label = ":ζ zeta:", insertText = "ζ", filterText = ":zeta:" },
+    { word = ":Zeta:", label = ":Ζ Zeta:", insertText = "Ζ", filterText = ":Zeta:" },
+    { word = ":eta:", label = ":η eta:", insertText = "η", filterText = ":eta:" },
+    { word = ":Eta:", label = ":Η Eta:", insertText = "Η", filterText = ":Eta:" },
+    { word = ":theta:", label = ":θ theta:", insertText = "θ", filterText = ":theta:" },
+    { word = ":Theta:", label = ":Θ Theta:", insertText = "Θ", filterText = ":Theta:" },
+    { word = ":iota:", label = ":ι iota:", insertText = "ι", filterText = ":iota:" },
+    { word = ":Iota:", label = ":Ι Iota:", insertText = "Ι", filterText = ":Iota:" },
+    { word = ":kappa:", label = ":κ kappa:", insertText = "κ", filterText = ":kappa:" },
+    { word = ":Kappa:", label = ":Κ Kappa:", insertText = "Κ", filterText = ":Kappa:" },
+  }
+end
+-- Λ       | λ     | Lambda
+-- Μ       | μ     | Mu
+-- Ν       | ν     | Nu
+-- Ξ       | ξ     | Xi
+-- Ο       | ο     | Omicron
+-- Π       | π     | Pi
+-- Ρ       | ρ     | Rho
+-- Σ       | σ     | Sigma
+-- Τ       | τ     | Tau
+-- Υ       | υ     | Upsilon
+-- Φ       | φ     | Phi
+-- Χ       | χ     | Chi
+-- Ψ       | ψ     | Psi
+-- Ω       | ω     | Omega
---
+++ plugin/cmp_greek.lua
@@ -0,0 +1,1 @@
+require("cmp").register_source("greek", require("cmp_greek").new())
